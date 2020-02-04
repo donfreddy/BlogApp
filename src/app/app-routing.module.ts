b46c1244906586be0d7ad7e38f72a04ec24b462c
Freddy Tamwo <55187739+Donfreddy@users.noreply.github.com>
2020-02-04 07:11:33 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-/* import { PostListComponent } from './post-list/post-list.component';
-import { PostListItemComponent } from './post-list-item/post-list-item.component';
- */
 
 const routes: Routes = [
-
- /*  { path: 'posts', component: PostListComponent },
-  { path: 'post/new', component: PostListItemComponent}
-   */
 ];
 
 @NgModule({
